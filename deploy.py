f783b011899c436160def4315ff05e4c90ac1d05
omojefe emesaha <emesahaomojefe@gmail.com>
2022-09-08 09:21:51 +0000

    my first commit

---
+++ deploy.py
@@ -0,0 +1,9 @@
+"Hello World"
+ = shell scripts
+python ('Hello World')
+#= python scripts  
+#echo "Hello World"
+#= shell scripts
+#python ('Hello World')
+#= python scripts
+print('Hello World')  
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "i love to work in Tesla cpy"
+date
